71c2403796983a704211dc2a22d442d065afbfd6
Aureo-Bueno <aureoalexandre@outlook.com>
2022-10-06 11:18:33 -0300

    adicionando contador de incremento de decremento de numeros

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 function App() {
+
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Contador: { counter }</h1>
+        <div>
+          <div>
+            <button onClick={(() => dispatch({type:"INCREMENT"}))}>INCREMENTO</button>
+          </div>
+          <div>
+            <button onClick={(() => dispatch({type:"DECREMENT"}))}>DECREMENTO</button>
+          </div>
+        </div>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = createStore(rootReducer);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Provider store={store}><App/></Provider>,document.getElementById('root'));
